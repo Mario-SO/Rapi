20d7bbe4ac8c21f95ab5952fe38cc440d71320c9
Mario-SO <gh_mario.proclaim@slmail.me>
2025-05-17 15:52:11 +0200

    Initial Commit

---
+++ Rapi/RapiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RapiApp.swift
+//  Rapi
+//
+//  Created by mario on 17/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RapiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RapiTests/RapiTests.swift
@@ -0,0 +1,17 @@
+//
+//  RapiTests.swift
+//  RapiTests
+//
+//  Created by mario on 17/5/25.
+//
+
+import Testing
+@testable import Rapi
+
+struct RapiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
